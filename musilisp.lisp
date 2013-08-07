3c4aa6a7e834e441ab8265935c5a8a133399a932
Bruno Burke <brunoburke@googlemail.com>
2013-08-07 10:57:45 +0200

    Writing Wavefile works now!
    Added Function to write numbers in x bytes.
    Searching for optimizations of the Data-Generator-Function

--- musilisp.lisp
+++ musilisp.lisp
@@ -1,6 +1,5 @@
 ;;; ToDo:
-;;; Integer to x bytes: 
-;;; countxbits --- Startbit beachten!
+
 
 ;;; =========================
 ;;; Wave File Structure
@@ -32,6 +31,14 @@
 ;;; <Block Align> Second Value
 ;;; <Block Align> .....
 
+(defun numbertoxbyte (number bytecount)
+	(if (> bytecount 0)
+		(let ((masked (logand number 255)))
+			(cons masked (numbertoxbyte (ash number -8) (1- bytecount)))
+		)
+		nil
+	)
+)
 
 (defun ltoi (charlist)
   (mapcar #'char-int charlist)
@@ -53,40 +60,64 @@
 (defun get-wave-header (filesize)
   (concatenate 'list
    (strtobyte "RIFF")
-   (list 0 161 127 228)
+   ;;;(list 0 161 127 228)
+	(list 228 127 161 0)
    (strtobyte "WAVE"))
 )
 
 (defun get-fmt-chunk ()
 	(concatenate 'list
 	(strtobyte "fmt ")
-	(list 0 0 0 16) ;;; Header Size
-	(list 0 1) 		;;; Format Tag = PCM
-	(list 0 1)		;;; Channels = Mono
-	(list 0 0 172 68) ;;; Sample-Rate = 44100hz
-	(list 0 1 88 136)		;;; Bytes per Second	
-	(list 0 2)		;;; Bytes per Sample (channels * ((bits+7)/8))
-	(list 0 16)		;;; Bits per Sample = 16
+	(numbertoxbyte 16 4) ;;; Header Size
+	(numbertoxbyte 1 2) ;;; Format Tag = PCM
+	(numbertoxbyte 1 2) ;;; Channels = Mono
+	(numbertoxbyte 44100 4) ;;; Sample-Rate = 44100hz
+	(numbertoxbyte 88200 4)		;;; Bytes per Second	
+	(numbertoxbyte 2 2) ;;; Bytes per Sample (channels * ((bits+7)/8))
+	(numbertoxbyte 16 2)		;;; Bits per Sample = 16
 	)
+)
+(defun make-mysin (frequency)
+(let ((pf2 (* frequency 2 pi)))
+    #'(lambda (x) 
+	(nth-value 0 (round (* (sin (* x pf2)) 32767)))
+	)
+	)
+) 
+(defun mysin (x frequency)
+	(nth-value 0 (round (* (sin (* x 440 2 pi)) 32767)))
 )
 
 (defun get-data-chunk ()
-	(concatenate 'list
+	(let ((xsin (make-mysin 440)))
+	(setf start (get-universal-time))
+	(setf listedata (concatenate 'list
 	(strtobyte "data")
-	(list 0 161 127 192)
+	(list 192 127 161 0)
 	(loop for i
 	from 1 
 	to 10584000
-	collect (random 255))
-	(loop for i
-	from 1 
-	to 10584000
-	collect (random 255))
-	)
+	;;;collect (* (mysin i 440) 128))
+	;;;collect (numbertoxbyte (random 550) 2))
+	collect (numbertoxbyte (funcall xsin i) 2))
+	))
+	(setf ende (get-universal-time))
+	listedata)
 )
 
 
 (defun testwrite ()
+	(with-open-file (s "testwave.wav"  :direction :output :element-type 'unsigned-byte)
+		(write-byte-sequence 
+		(concatenate 'list
+		(get-wave-header 0)
+		(get-fmt-chunk)
+		(get-data-chunk)) s)
+	)
+	(- ende start)
+)
+
+(defun testwrite2 ()
 	(with-open-file (s "testwave.wav"  :direction :output :element-type 'unsigned-byte)
 		(write-byte-sequence 
 		(concatenate 'list
